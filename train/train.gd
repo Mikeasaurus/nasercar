19141e3a8b80aac9387da6236f201bfb63c6cf2e
Mikeasaurus <161517199+Mikeasaurus@users.noreply.github.com>
2025-12-10 00:36:50 -0500

    Some updates to the train.
    Use a larger radius of curvature for train tracks.  Trains don't like sudden 90
    degree changes in direction. XD
    
    Fixed length/width ratio for train cars, and adjusted the design to look more
    like a train and less like a box.
    
    Adjusted the logic for connecting the cars along the track and adjusting
    train car orientation.  The cars should stay connected better now, and stay
    on the tracks better when going around bends.

--- train/train.gd
+++ train/train.gd
@@ -18,13 +18,17 @@ func _get_configuration_warnings() -> PackedStringArray:
 	return []
 
 func _ready() -> void:
-	# Add trains to track.
+	# Add path following for trains.
+	# Each PathFollow2D represents the position of the connectors between trains
+	# (and very front and back).
 	var path: Path2D = _get_trackpath()
-	for i in range(num_cars):
+	for i in range(num_cars+1):
 		var p: PathFollow2D = PathFollow2D.new()
 		p.rotates = false
 		path.add_child.call_deferred(p,true)
 		trackpaths.append(p)
+	# Add trains to track.
+	for i in range(num_cars):
 		var t
 		if i == 0:
 			t = load("res://train/train_front.tscn").instantiate()
@@ -37,21 +41,11 @@ func _process(delta: float) -> void:
 	var old_angles: Array[float]
 	for i in range(num_cars):
 		old_angles.append(traincars[i].global_rotation)
-	#if trackpaths[0].progress > 100: return
-	trackpaths[0].progress += delta * 100
-	# Update position of train cars.
-	for i in range(1,num_cars):
-		trackpaths[i].progress = trackpaths[i-1].progress - 64
+	# Update positions of train car connectors.
+	trackpaths[0].progress += delta * 200
+	for i in range(1,num_cars+1):
+		trackpaths[i].progress = trackpaths[i-1].progress - 100
+	# Update train cars to align with the connectors.
 	for i in range(num_cars):
-		traincars[i].position = trackpaths[i].position
-	# Update orientation of train cars along the track.
-	# Keep joints as close together as possible
-	var l: int = 25
-	for i in range(num_cars):
-		var p1: Vector2 = traincars[i].position
-		var p2: Vector2 = traincars[i].position
-		if i > 0:
-			p1 = traincars[i-1].position + l * Vector2.from_angle(traincars[i-1].rotation)
-		if i < num_cars-1:
-			p2 = traincars[i+1].position - l * Vector2.from_angle(traincars[i+1].rotation)
-		traincars[i].rotation = (p2-p1).angle()
+		traincars[i].position = (trackpaths[i].position + trackpaths[i+1].position) / 2
+		traincars[i].global_rotation = (trackpaths[i+1].position - trackpaths[i].position).angle()
