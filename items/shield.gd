32918227df3e71b89a230a7846c670c60d57e948
Mikeasaurus <161517199+Mikeasaurus@users.noreply.github.com>
2025-12-21 17:23:40 -0500

    Fix an error in the logs caused by client-side despawning of shield in multiplayer games.
    Despawning should only happen on server side.
    
    Doesn't effect gameplay, but just wanted to keep logs as clean as possible.
    
    There's still some other despawn-related message in the logs that I'm trying
    to track down, but this takes care of one of them at least.

--- items/shield.gd
+++ items/shield.gd
@@ -5,12 +5,6 @@ class_name Shield
 @export var current_rotation: float
 
 func _ready() -> void:
-	# Dynamically add the shield elements, so they're registered with the MultiplayerSynchronizer
-	# (so they can be properly despawned on clients after an inpact).
-	#for i in range(6):
-	#	var beat: Beat = load("res://items/beat.tscn").instantiate()
-	#	$Beats.add_child(beat)
-	#	beat.position = 150 * Vector2.from_angle(PI/3*i)
 	# Fade in the shield
 	modulate = Color.hex(0xffffff00)
 	var tween: Tween = create_tween()
@@ -23,4 +17,5 @@ func _process(_delta: float) -> void:
 # Clean up after all beats have been used.
 func _on_beats_child_exiting_tree(_node: Node) -> void:
 	var n: int = len($Beats.get_children())
-	if n == 1: queue_free()
+	if n == 1 and multiplayer.get_unique_id() == 1:
+		queue_free()
