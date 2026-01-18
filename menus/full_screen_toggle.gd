7c7dae97466c4b8e4f1ecba156210437fd7c942c
Mikeasaurus <161517199+Mikeasaurus@users.noreply.github.com>
2026-01-18 16:35:50 -0500

    Work around for a weird bug where I had to click the fullscreen toggle twice to make it go into windowed mode.
    First time the setting doesn't "stick"?
    
    This used to work in Godot 4.3 (as far as I remember), but seems to have
    been broken in a more recent version (4.4 or 4.5?)

--- menus/full_screen_toggle.gd
+++ menus/full_screen_toggle.gd
@@ -5,3 +5,7 @@ func _on_button_pressed() -> void:
 		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
 	else:
 		DisplayServer.window_set_mode(DisplayServer.WindowMode.WINDOW_MODE_WINDOWED)
+		# Fix a bug where it doesn't actually go to windowed mode the first time
+		# you click from full screen.  This mimicks clicking it a second time (which works???)
+		await get_tree().create_timer(0.1).timeout
+		DisplayServer.window_set_mode(DisplayServer.WindowMode.WINDOW_MODE_WINDOWED)
